27c6007f93ee7c4538644d7a3b548b9b7c46ec33
angad <angadreact01@gmail.com>
2021-12-30 09:41:26 -0700

    first commit

---
+++ FUNCTION_INSIDE_FUNCTION.c
@@ -0,0 +1,26 @@
+// FUNCTION INSIDEGithu
+#include <stdio.h>
+void Goodmorning();
+void Goodafternoon();
+void Goodnight();
+int main()
+{
+    Goodmorning();
+
+    return 0;
+}
+void Goodmorning()
+
+{
+    printf("Good morning Github\n");
+    Goodafternoon();
+}
+void Goodafternoon()
+{
+    printf("Good afternoon Github\n");
+    Goodnight();
+}
+void Goodnight()
+{
+    printf("Good night Github\n");
+}
